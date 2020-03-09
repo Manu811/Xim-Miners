0783f8dfd9884dd4cb82db2466d14c6a60c6f2dd
Manuel Urgell <54177784+Manu811@users.noreply.github.com>
2020-03-08 22:43:15 -0600

    'v1'

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour{
+    Rigidbody2D player;
+    float movementx, movementy;
+    public float speed;
+    public int lifes;
+
+    // Start is called before the first frame update
+    void Start(){
+        player = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        movementx = Input.GetAxis("Horizontal")*speed;
+        player.MovePosition(new Vector2(player.position.x + movementx, player.position.y));
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        movementy = Input.GetAxis("Vertical");
+        if (movementy < 0){
+            if (collision.transform.CompareTag("Dirt")){
+                Destroy(collision.gameObject);
+            }
+        }
+        
+    }
+}
