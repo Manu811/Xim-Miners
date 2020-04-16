30f852d433c6be7bc1ea9315d92b59cb83d10469
XimeHut <50258523+XimeHQ@users.noreply.github.com>
2020-04-15 23:16:01 -0500

    Menu2

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Pausa: MonoBehaviour
+{
+    public GameObject toggle;
+
+
+
+
+
+    public void Jugar()
+    {
+        Debug.Log("Continuar Jugando");
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Ir al Menu");
+    }
+
+
+
+
+}
